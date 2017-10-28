88b6d4531fb3ff753ca9bc6925f239c56a595208
TateIsuKanna <TateIsuKanna@users.noreply.github.com>
2017-10-28 15:53:20 +0900

    実際に使う版

--- fs.py
+++ fs.py
@@ -2,14 +2,15 @@ import RPi.GPIO as GPIO
 from tkinter import *
 import datetime
 import pygame.mixer
+import time
 
-stopwatch_signal_channel=11
-sound_signal_channel=12
+stopwatch_signal_channel=37
+sound_signal_channel=38
 
 is_timer_enable=False#本当は使わずに済ませたい
 
 pygame.mixer.init()
-pygame.mixer.music.load("/home/pi/Voices.mp3")
+pygame.mixer.music.load("/home/pi/フラッシュザウルス.mp3")
 
 def stopwatch_signal_callback(channel):
 	if GPIO.input(channel):
@@ -35,6 +36,9 @@ def update_stopwatch_callback():
 
 def sound_signal_callback(channel):
 	pygame.mixer.music.play()
+	time.sleep(30)
+	pygame.mixer.music.stop()
+
 
 root = Tk()
 root.attributes("-fullscreen", True)
@@ -44,7 +48,7 @@ stopwatch_label = Label(root,textvariable=stopwatch_label_text,font = ("",40))
 stopwatch_label.pack(expand=1)
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup([stopwatch_signal_channel,sound_signal_channel], GPIO.IN)
+GPIO.setup([stopwatch_signal_channel,sound_signal_channel], GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(stopwatch_signal_channel, GPIO.BOTH, callback=stopwatch_signal_callback) 
 GPIO.add_event_detect(sound_signal_channel, GPIO.RISING, callback=sound_signal_callback) 
 
